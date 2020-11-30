16d5868a580a55231cdf4b41de0a046cd573a956
David Nelson <eatdrinksleepcode@gmail.com>
2020-11-29 21:42:32 -0600

    Link peripherals via bus

--- visualizer/items-bus.js
+++ visualizer/items-bus.js
@@ -1,14 +1,3 @@
-const busItems = [
-    'iron-plate',
-    'copper-plate',
-    'steel-plate'
-];
-
-const alternateImages = {
-    "battery-equipment": ["personal-battery"]
-    // "battery-mk2-equipment": ["personal-battery-mk2"]
-};
-
 const excludedRecipesPatterns = [
     /empty-.+-barrel/,
     /fill-.+-barrel/
@@ -36,28 +25,46 @@ function splitRecipesByProduct(recipes) {
         .groupBy(x => x.productName, x => x.recipe);
 }
 
-class ItemList {
-    constructor(recipes) {
-        this.recipes = recipes;
-        this.recipesByProduct = splitRecipesByProduct(this.recipes);
+recipesByProduct = splitRecipesByProduct(recipes);
+
+const peripherals = {
+    "automation-science": {
+        produces: {
+            "automation-science-pack": 1
+        }
+    },
+    "logistic-science": {
+        produces: {
+            "logistic-science-pack": 1
+        }
+    }
+};
+
+class Peripheral {
+    constructor(name, produces, inputBus, outputBus) {
+        this.name = name;
+        this.produces = [].concat(produces);
         this.items = {};
-        this.reduceTargetRecipes();
+        this.inputBus = inputBus;
+        this.outputBus = outputBus;
+        this.reduceOutputs();
+        this.outputs = this.items; // HACK
     }
 
-    reduceTargetRecipes() {
-        [
-            "research",
-            "rocket-launch"
-        ].forEach(productName => this.reduceRecipesForProduct(productName));
+    reduceOutputs() {
+        this.produces.forEach(productName => {
+            this.outputBus.accept(this.reduceRecipesForProduct(productName));
+        });
     }
 
     reduceRecipesForProduct(productName) {
-        this.initItem(productName);
-        const recipesForProduct = this.recipesByProduct[productName];
+        const product = this.initItem(productName);
+        const recipesForProduct = recipesByProduct[productName];
         console.log({recipesForProduct});
-        if(!recipesForProduct) return;
-        console.assert(recipesForProduct.length == 1, {productName, recipesForProduct});
+        console.assert(recipesForProduct?.length > 0, "No recipe found", {productName});
+        console.assert(recipesForProduct.length == 1, "More than 1 recipe found", {productName, recipesForProduct});
         this.reduceRecipe(recipesForProduct[0]);
+        return product;
     }
 
     reduceRecipe(recipe) {
@@ -65,24 +72,82 @@ class ItemList {
             const product = this.initItem(productName);
             product.recipe = recipe;
             product.originalRecipe = recipe; // for compatibility
-            product.ingredients = recipe.ingredients;
             product.isIncluded = true;
-            Object.keys(product.ingredients).forEach(ingredientName => {
-                this.reduceRecipesForProduct(ingredientName);
+            Object.keys(recipe.ingredients).forEach(ingredientName => {
+                if(this.inputBus.includes(ingredientName)) { 
+                    product.ingredients[this.inputBus.find(ingredientName).displayName] = recipe.ingredients[ingredientName]
+                } else {
+                    product.ingredients[this.reduceRecipesForProduct(ingredientName).displayName] = recipe.ingredients[ingredientName];
+                }
             });
         });
     }
 
     initItem(name) {
-        if(!this.items[name]) {
-            this.items[name] = {
+        const localName = name + "-" + this.name;
+        if(!this.items[localName]) {
+            this.items[localName] = {
                 name,
-                displayName: name,
-                images: alternateImages[name],
-                ingredients: {}
+                displayName: localName,
+                ingredients: {},
+                isIncluded: true
             };
         }
-        return this.items[name];
+        return this.items[localName];
+    }
+}
+
+class Bus {
+    constructor(outputs) {
+        this.outputs = {};
+        (outputs || []).forEach(output => {
+            this.accept(output).ingredients = {};
+        });
     }
 
-};
+    makeLocalName(itemName) {
+        return itemName + "-bus";
+    }
+
+    accept(output) {
+        const name = output.name;
+        const localName = this.makeLocalName(name);
+        this.outputs[localName] = {
+            name,
+            displayName: localName,
+            ingredients: { [output.displayName]: 1 },
+            isIncluded: true,
+            recipe: {},
+            originalRecipe: {} // for compatibility
+        };
+        return this.outputs[localName];
+    }
+
+    includes(itemName) {
+        return this.outputs[this.makeLocalName(itemName)];
+    }
+
+    find(itemName) {
+        return this.outputs[this.makeLocalName(itemName)];
+    }
+}
+
+function naturalItem(name) {
+    return {
+        name,
+        displayName: name,
+        ingredients: {},
+        isIncluded: true,
+        recipe: {},
+        originalRecipe: {} // for compatibility
+    };
+}
+
+const oreBus = new Bus([naturalItem("iron-ore"), naturalItem("copper-ore")]);
+const mainBus = new Bus()
+forge = new Peripheral("forge", ["iron-plate", "copper-plate"], oreBus, mainBus)
+allItems = [
+    oreBus,
+    forge,
+    mainBus
+].reduce((x, y) => Object.assign(x, y.outputs), {});
